cfcd4493fe7559dafd464a059e89a67ab3a48983
Peter Levett$ git config --global user.name Peter <peterlevett@gmail.com>
2017-04-15 12:58:26 -0700

    classified MethodMaker_functions.

--- Symplify/MethodMaker.py
+++ Symplify/MethodMaker.py
@@ -2,7 +2,8 @@ import MethodMaker_functions as mm
 from sys import argv
 MethodMaker, TLLfile = argv
 
-line_list = mm.header_maker("Peter", 1, "Peter", 1)
+method = mm.MethodMakerFunctions(TLLfile)
+method.header_maker("Peter", 1, "Peter", 1)
 
 #Procedure
 # vial options: '8;Vial 1-1', '8;Vial 1-2', '8;Vial 1-3'
@@ -14,23 +15,23 @@ volume = 1.00
 x_coord = 0
 
 while x_coord < 12:
-    line_list = mm.step_maker(line_list, "8;Vial 1-1", volume, [x_coord, 0], "N")
+    method.step_maker("8;Vial 1-1", volume, [x_coord, 0], "N")
     volume += float(1.00)
     x_coord += 1
 else:
-    line_list = mm.step_maker(line_list, "8;Vial 1-1", volume, [x_coord, 0], "Y")
+    method.step_maker("8;Vial 1-1", volume, [x_coord, 0], "Y")
 
 vial_2_volume = 13.00
 x_coord = 0
 
 while x_coord < 12:
-    line_list = mm.step_maker(line_list, "8;Vial 1-2", vial_2_volume, [x_coord, 0], "N")
+    method.step_maker("8;Vial 1-2", vial_2_volume, [x_coord, 0], "N")
     volume -= float(1.00)
     x_coord += 1
     vial_2_volume -= 1
 else:
-    line_list = mm.step_maker(line_list, "8;Vial 1-2", vial_2_volume, [x_coord, 0], "Y")
+    method.step_maker("8;Vial 1-2", vial_2_volume, [x_coord, 0], "Y")
 
-mm.file_and_report_maker(line_list, TLLfile)
+method.file_and_report_maker()
 
 
--- Symplify/MethodMaker_functions.py
+++ Symplify/MethodMaker_functions.py
@@ -8,30 +8,35 @@ from HeaderGenerator import HeaderGenerator
 from TLLGenerator import TLLGenerator
 
 
-def header_maker(group_name, group_number, test_name, test_number):
-    header = HeaderGenerator()
-    header_line = header.header_generator()
-    group = GroupGenerator(group_name, group_number)
-    group_line = group.group_generator()
-    test = TestGenerator(test_name, test_number)
-    test_line = test.test_generator()
-    step = StepGenerator(256)
-    step_type_line = step.step_generator()
-    filler_line = step.filler_line_generator()
-    return [header_line, group_line, test_line, step_type_line, filler_line]
+class MethodMakerFunctions:
 
+    def __init__(self, TLLfile, linelist=[]):
+        self.linelist = linelist
+        self.TLLfile = TLLfile
 
-def step_maker(header_list, vial, volume, drop_coordinates, drop_tip):
-    draw = DrawGenerator(volume, vial)
-    draw_line = draw.draw_generator()
-    drop = DropGenerator(volume, '4;', drop_coordinates, drop_tip)
-    drop_line = drop.drop_generator()
-    header_list.append(draw_line)
-    header_list.append(drop_line)
-    return header_list
+    def header_maker(self, group_name, group_number, test_name, test_number):
+        header = HeaderGenerator()
+        header_line = header.header_generator()
+        group = GroupGenerator(group_name, group_number)
+        group_line = group.group_generator()
+        test = TestGenerator(test_name, test_number)
+        test_line = test.test_generator()
+        step = StepGenerator(256)
+        step_type_line = step.step_generator()
+        filler_line = step.filler_line_generator()
+        self.linelist = [header_line, group_line, test_line, step_type_line, filler_line]
+        return self.linelist
 
+    def step_maker(self, vial, volume, drop_coordinates, drop_tip):
+        draw = DrawGenerator(volume, vial)
+        draw_line = draw.draw_generator()
+        drop = DropGenerator(volume, '4;', drop_coordinates, drop_tip)
+        drop_line = drop.drop_generator()
+        self.linelist.append(draw_line)
+        self.linelist.append(drop_line)
+        return self.linelist
 
-def file_and_report_maker(line_list, TLLfile):
-    file_generator = TLLGenerator(line_list, TLLfile)
-    file_generator.tll_generator()
-    file_generator.report_generator()
+    def file_and_report_maker(self):
+        file_generator = TLLGenerator(self.linelist, self.TLLfile)
+        file_generator.tll_generator()
+        file_generator.report_generator()
