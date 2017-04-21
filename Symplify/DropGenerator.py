917b4284b916590bc907aa8edfade443dc611e04
Peter Levett$ git config --global user.name Peter <peterlevett@gmail.com>
2017-04-21 16:38:21 -0700

    Added help file for DropGenerator. Note that there currently is divergence between the project on the computer in the lab and this version of the project. If you pull onto the lab computer from this commit, you'll lose the changes you made to the TLL generator code.

--- Symplify/DropGenerator.py
+++ Symplify/DropGenerator.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
-class DropGenerator():
+class DropGenerator:
+    """
+    This class generates a drop line. Each draw line must be followed by a drop line.
+
+    Method Hierarchy:
+
+    drop_generator
+        generate_known_variables
+            drop_or_not
+        generate_unknown_variables
+        generate_drop_line
+    """
 
     def __init__(self, drop_volume, drop_location, drop_coordinates, change_tip):
+        """This class receives 4 variables: drop_volume (the amount, in microlitres, to be dropped
+        into the drop location at the drop coordinates), drop_location (the tray the volume is being dropped in to),
+        drop_coordinates (the coordinates on the tray the volume is being dropped into) and change_tip - "Y" indicates that
+        the tip should be ejected and changed after the volume drop, "N" indicates that the tip shouldn't be ejected.
+
+        the drop_line is the string of code that Symbiot reads."""
         self.drop_volume = drop_volume
         self.drop_location = drop_location
         self.drop_coordinates = drop_coordinates
@@ -9,6 +26,8 @@ class DropGenerator():
         self.drop_line = 'string'
 
     def drop_generator(self):
+        """This function generates the known variables, then generates the unknown variables. It uses these to generate
+        the drop line, updates the drop line, and returns the drop_line."""
         known_variables = self.generate_known_variables()
         known_and_unknown_variables = self.generate_unknown_variables(known_variables)
         drop_line = self.generate_drop_line(known_and_unknown_variables)
@@ -16,6 +35,10 @@ class DropGenerator():
         return drop_line
 
     def generate_known_variables(self):
+        """This function creates the correct strings representing each known variable. known_variables_1 contains the
+        information for the volume and the location. known_variables_2 contains the coordinates. known_variables_3
+        contains information on whether or not to exchange the tip post-drop. These are then combined into a list and
+        returned."""
         known_variables_1 = "C;"+str(self.drop_volume)+";"+str(self.drop_location)
         known_variables_2 = str(self.drop_coordinates[0])+";"+str(self.drop_coordinates[1])
         known_variables_3 = self.drop_or_not()
@@ -23,6 +46,14 @@ class DropGenerator():
         return known_variables
 
     def drop_or_not(self):
+        """
+        This function contains an if statement that returns the appropriate code for each condition (Y or N). If
+        neither Y or N is detected, the function returns the least damaging instruction - to change the tip, which
+        reduces the risk of the test failing.
+
+        note that the 6 digit code accompanying the 1 or the 0 is technically unknown. The '70' is for drop and '62'
+        is for don't drop, but the other 4 numbers are anybody's guess.
+        """
         if self.change_tip == "Y":
             return ["262170", "1"]
         elif self.change_tip == "N":
@@ -31,6 +62,8 @@ class DropGenerator():
             return ["262170", "1"]
 
     def generate_unknown_variables(self, known_variables):
+        """This function first generates the three unknown variables, combines them with the known ones, and returns
+        all variables as a list - the order of this list is important."""
         unknown_four = 0
         unknown_five = 1
         unknown_seven = 0
@@ -44,5 +77,6 @@ class DropGenerator():
         return known_and_unknown_variables
 
     def generate_drop_line(self, known_and_unknown_variables):
+        """This function generates the actual line of code Symbiot reads, it joins the variables up with semicolons."""
         drop_line = ';'.join(known_and_unknown_variables)+';'
         return drop_line
